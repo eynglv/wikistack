5a00dde33e49ca14008744156c22a9a52d886128
Elvy Yang <hyang6@wellesley.edu>
2021-04-30 00:44:09 +0800

    added schema

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require ("express");
+const express = require("express");
 const morgan = require("morgan");
 const app = express();
 const layout = require("./views/layout");
-const { db } = require('./models');
+const { db, Page, User } = require("./models");
 
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
 
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({extended: false}));
+app.get("/", (req, res) => {
+  res.send(layout("this is confusing"));
+});
 
-
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database');
-  })
-  
-app.get("/", (req, res) =>{
-    res.send(layout("this is confusing"));
-})
+const init = async () => {
+  await Page.sync();
+  await User.sync();
+};
 
 app.listen(3000, () => {
-    console.log("listening")
-})
+  console.log("listening");
+});
+
+init();
